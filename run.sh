f182ea5345cddb8c75f6d9522f3097542497602f
lagoon0o0 <lagoonzxy@gmail.com>
2019-11-21 20:43:31 -0500

    update sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "flag: $1"
-git status
+echo "on the branch:"
+git branch | grep \* | cut -d ' ' -f2 
 cd liblfds7.1.1/liblfds711/build/gcc_gnumake/
 make clean
 make $1
 sudo make so_uninstall
 sudo make so_install 
+cd ../../../../
