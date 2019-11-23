a2f606ed69ea2c8db72b3c8c53f99fb83e85e3b5
lagoon0o0 <lagoonzxy@gmail.com>
2019-11-23 02:36:25 -0500

    Revert "update armada with mask"
    This reverts commit 11f449adb5ad9b5417f81cc957067aeaae2c4e23.

--- liblfds7.1.1/liblfds711/src/lfds711_queue_bounded_singleproducer_singleconsumer/lfds711_queue_bounded_singleproducer_singleconsumer_dequeue.c
+++ liblfds7.1.1/liblfds711/src/lfds711_queue_bounded_singleproducer_singleconsumer/lfds711_queue_bounded_singleproducer_singleconsumer_dequeue.c
@@ -42,16 +42,17 @@ int lfds711_queue_bss_dequeue( struct lfds711_queue_bss_state *qbsss,
 }
 
 
-uint32 dequeue(struct QbssState* state, uint32* k, uint32* v)
+uint32 armada_dequeue(struct armada_QbssState* state, uint32* k, uint32* v)
 {
+  LFDS711_MISC_BARRIER_LOAD;
   uint32 ret = 0;
   uint32 _4_write__index;
   _4_write__index = (*(state)).write_index;
-  uint32 _5_mask;
-  _5_mask = (*(state)).mask;
+  uint32 _5_array__size;
+  _5_array__size = (*(state)).array_size;
   uint32 _6_read__index;
   _6_read__index = (*(state)).read_index;
-  struct BSSQueueElement* _7_e = NULL;
+  struct armada_BSSQueueElement* _7_e = NULL;
   uint32 _8_key = 0;
   uint32 _9_value = 0;
   if ((_6_read__index) != (_4_write__index)) {
@@ -64,7 +65,8 @@ uint32 dequeue(struct QbssState* state, uint32* k, uint32* v)
       _9_value = (*(_7_e)).value;
       *v = _9_value;
     }
-    (*(state)).read_index = ((_6_read__index) + ((uint32)1)) & (_5_mask);
+    (*(state)).read_index = ((_6_read__index) + ((uint32)1)) % (_5_array__size);
+    LFDS711_MISC_BARRIER_STORE;
     ret = (uint32)1;
     return ret;
   }
